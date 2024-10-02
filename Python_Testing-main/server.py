4bf5372a5fa40df00fdf765fada4aaf5e1e89052
abbydjomoloïque <djomoloicabby@gmail.com>
2024-10-02 09:45:56 +0000

    logique book

--- Python_Testing-main/server.py
+++ Python_Testing-main/server.py
@@ -37,12 +37,10 @@ def showSummary():
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
+    foundClub = next((c for c in clubs if c['name'] == club), None)
+    foundCompetition = next((c for c in competitions if c['name'] == competition), None)
+    if foundClub is None or foundCompetition is None:
+        flash("Something went wrong - please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
